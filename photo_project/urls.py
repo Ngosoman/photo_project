9b822d3e3a7ab9101322c5a37c1fcfe6fbbd6191
Tom Mwambingu <[tomchom30@gmail.com]>
2025-08-13 12:21:14 +0300

    Linked login paths

--- photo_project/urls.py
+++ photo_project/urls.py
@@ -19,4 +19,7 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
+    path('register/', views.register, name='register'),
 ]
