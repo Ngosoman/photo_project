1f437f49c81effc9e1453d4571ff47b10247b13b
Tom Mwambingu <[tomchom30@gmail.com]>
2025-08-13 12:11:52 +0300

    Created photo class in models

--- photo_gallery/models.py
+++ photo_gallery/models.py
@@ -2,3 +2,12 @@
 # Create your models here.
 from django.db import models
 from django.contrib.auth.models import User
+
+class Photo(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    image = models.ImageField(upload_to='photos/')
+    tags = models.CharField(max_length=200, blank=True)
+    uploaded_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name="photos")
+    likes = models.ManyToManyField(User, related_name="liked_photos", blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
