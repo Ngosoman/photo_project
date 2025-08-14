8e9093a6a442a3ff0f7507e8ab3928bf20423bc0
Tom Mwambingu <[tomchom30@gmail.com]>
2025-08-14 21:05:06 +0300

    Add gallery home view and update URL configuration

--- photo_gallery/views.py
+++ photo_gallery/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.models import User
 from django.contrib import messages
+from django.shortcuts import render
+
+def gallery_home(request):
+    return render(request, 'gallery_home.html')
 
 
 def register(request):
