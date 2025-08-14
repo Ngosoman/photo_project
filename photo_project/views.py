00cf80d9ff2ee974a1bc8e0c4bf01f4a729b28b5
Tom Mwambingu <[tomchom30@gmail.com]>
2025-08-14 20:27:31 +0300

    Created views.py

---
+++ photo_project/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from .forms import RegistrationForm
+from django.contrib.auth.forms import AuthenticationForm
+
+def register(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.set_password(form.cleaned_data['password'])
+            user.save()
+            login(request, user)
+            return redirect('gallery_home')
+    else:
+        form = RegistrationForm()
+    return render(request, 'templates/register.html', {'form': form})
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('gallery_home')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'templates/login.html', {'form': form})
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
