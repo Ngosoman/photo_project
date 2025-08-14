4e0b3c520776810c47cf1b009e261e69cfa01fd9
Tom Mwambingu <[tomchom30@gmail.com]>
2025-08-14 20:24:36 +0300

    Created and Updated forms.py

---
+++ photo_project/forms.py
@@ -0,0 +1,18 @@
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import AuthenticationForm
+
+class RegistrationForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput)
+    confirm_password = forms.CharField(widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+
+    def clean(self):
+        cleaned = super().clean()
+        pw = cleaned.get("password")
+        cpw = cleaned.get("confirm_password")
+        if pw != cpw:
+            self.add_error('confirm_password', "Passwords must match")
