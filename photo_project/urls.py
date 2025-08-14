2482b4b9dc252b8ce44ca94be46251cdc3a4623f
Tom Mwambingu <[tomchom30@gmail.com]>
2025-08-14 20:29:24 +0300

    Updated urls

--- photo_project/urls.py
+++ photo_project/urls.py
@@ -22,4 +22,7 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
     path('register/', views.register, name='register.html', name='register'),
+    path('accounts/', include('accounts.urls')),
+    path('', include('photo_gallery.urls')),  # gallery homepage
+
 ]
