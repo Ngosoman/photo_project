aaa6880b88a7039810954e6b7521f8d0af320252
Tom Mwambingu <[tomchom30@gmail.com]>
2025-08-19 21:20:48 +0300

    Updated Models to allow uploading

--- photo_gallery/models.py
+++ photo_gallery/models.py
@@ -1,5 +1,3 @@
-
-# Create your models here.
 from django.db import models
 from django.contrib.auth.models import User
 
@@ -10,9 +8,10 @@ class Photo(models.Model):
     tags = models.CharField(max_length=200, blank=True)
     uploaded_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name="photos")
     likes = models.ManyToManyField(User, related_name="liked_photos", blank=True)
-    created_at = models.DateTimeField(auto_now_add=True)
+    uploaded_at = models.DateTimeField(auto_now_add=True)
 
     def total_likes(self):
         return self.likes.count()
+
     def __str__(self):
         return self.title
